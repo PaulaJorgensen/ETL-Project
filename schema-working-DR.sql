f0d2091b74124eb5f483b1e998695cb32c76bffb
delran531 <52218698+delran531@users.noreply.github.com>
2019-12-12 20:15:15 -0500

    Create schema-working-DR.sql

---
+++ schema-working-DR.sql
@@ -0,0 +1,42 @@
+drop table population_by_zip;
+CREATE TABLE population_by_zip (
+population integer,
+min_age integer,
+max_age integer,
+gender varchar (10),
+zip_code varchar (5),
+geo_id varchar (50)
+);
+
+-- Import population_by_zip file
+-- Aggregate population data by zip code. Drop gender and age info
+create table pop_zip_agg as (
+select zip_code
+	, sum(population) population
+from public.population_by_zip
+group by zip_code
+order by zip_code);
+
+-- Clean fast_food_datafiniti to trim zip codes longer than 5 characters
+update public.fast_food_datafiniti
+set postal_code = left(postal_code, 5) 
+where length(postal_code) > 5;
+
+--Join to fast food datafiniti dataset and create new table
+-- (28 don't match becasue the population/zip data is census based and over 9 years old)
+create table fast_food_population_base as (
+select ff.name
+	, ff.address
+	, ff.city
+	, ff.country
+	, ff.postal_code
+	, ff.province "state"
+	, ff.websites "website"
+	, p.population
+from public.fast_food_datafiniti ff
+join pop_zip_agg p on ff.postal_code = p.zip_code
+order by 1,3);
+
+--new table output
+select *
+from fast_food_population_base;
